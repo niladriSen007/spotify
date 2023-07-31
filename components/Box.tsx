e83d40fb981e2a44b59f6be812adfc27d29db077
Niladri <niladrisengit12@gmail.com>
2023-07-31 13:54:28 +0530

    day-02

---
+++ components/Box.tsx
@@ -0,0 +1,21 @@
+import { twMerge } from "tailwind-merge";
+interface BoxProps{
+    children:React.ReactNode,
+    className ?: string
+}
+
+const Box : React.FC<BoxProps> = ({ children,className }) => {
+  return (
+    <div  className={twMerge(
+        `
+        bg-[#101B30]
+        rounded-lg 
+        h-fit 
+        w-full
+        `, 
+        className
+      )}>{children}</div>
+  )
+}
+
+export default Box
